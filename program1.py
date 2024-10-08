e27e4619aa340f2e55f5585aaf63d6fc73ac8f49
Amanm12 <0111ec211067.pooja@gmail.com>
2024-10-08 16:29:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -16,3 +16,8 @@ class Solution(object):
                 stack.append(char)
 
         return not stack
+
+# Example usage:
+solution = Solution()
+print(solution.isValid("{[()]}"))  # Output: True
+print(solution.isValid("{[(])}"))  # Output: False
